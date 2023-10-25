57d38bc34f66b8a0da6e7be2ebb1c1512f52b370
Arsénio Muanda <66343929+arseniomuanda@users.noreply.github.com>
2023-10-25 20:35:45 +0100

    primeiro commit

--- app/Views/login/login.php
+++ app/Views/login/login.php
@@ -1,118 +1,115 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-
-                  <title>DashboardKit Bootstrap 5 Admin Template</title>
-                  <!-- HTML5 Shim and Respond.js IE11 support of HTML5 elements and media queries -->
-                  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-                  <!--[if lt IE 11]>
-		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
-                  <!-- Meta -->
-                  <meta charset="utf-8">
-                  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-                  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                  <meta name="description"
-                                    content="DashboardKit is made using Bootstrap 5 design framework. Download the free admin template & use it for your project.">
-                  <meta name="keywords"
-                                    content="DashboardKit, Dashboard Kit, Dashboard UI Kit, Bootstrap 5, Admin Template, Admin Dashboard, CRM, CMS, Free Bootstrap Admin Template">
-                  <meta name="author" content="DashboardKit ">
-
-
-                  <!-- Favicon icon -->
-                  <link rel="icon" href="/assets/images/favicon.svg" type="image/x-icon">
-
-                  <!-- font css -->
-                  <link rel="stylesheet" href="/assets/fonts/feather.css">
-                  <link rel="stylesheet" href="/assets/fonts/fontawesome.css">
-                  <link rel="stylesheet" href="/assets/fonts/material.css">
-
-                  <!-- vendor css -->
-                  <link rel="stylesheet" href="/assets/css/style.css" id="main-style-link">
-
-
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--favicon-->
+    <link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
+    <!--plugins-->
+    <link href="assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
+    <link href="assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
+    <link href="assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+    <!-- loader-->
+    <link href="assets/css/pace.min.css" rel="stylesheet" />
+    <script src="assets/js/pace.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-extended.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link href="assets/css/app.css" rel="stylesheet">
+    <link href="assets/css/icons.css" rel="stylesheet">
+    <title>Login</title>
 </head>
 
-<!-- [ auth-signin ] start -->
-<div class="auth-wrapper">
-                  <div class="auth-content">
-                                    <div class="card">
-                                                      <div class="row align-items-center text-center">
-                                                                        <div class="col-md-12">
-                                                                                          <div class="card-body">
-                                                                                                            <img src="/assets/images/logo-dark.svg"
-                                                                                                                              alt=""
-                                                                                                                              class="img-fluid mb-4">
-                                                                                                            <h4
-                                                                                                                              class="mb-3 f-w-400">
-                                                                                                                              Signin
-                                                                                                            </h4>
-                                                                                                            <div
-                                                                                                                              class="input-group mb-3">
-                                                                                                                              <span
-                                                                                                                                                class="input-group-text"><i
-                                                                                                                                                                  data-feather="mail"></i></span>
-                                                                                                                              <input type="email"
-                                                                                                                                                class="form-control"
-                                                                                                                                                placeholder="Email address">
-                                                                                                            </div>
-                                                                                                            <div
-                                                                                                                              class="input-group mb-4">
-                                                                                                                              <span
-                                                                                                                                                class="input-group-text"><i
-                                                                                                                                                                  data-feather="lock"></i></span>
-                                                                                                                              <input type="password"
-                                                                                                                                                class="form-control"
-                                                                                                                                                placeholder="Password">
-                                                                                                            </div>
-                                                                                                            <div
-                                                                                                                              class="form-group text-left mt-2">
-                                                                                                                              <div
-                                                                                                                                                class="form-check">
-                                                                                                                                                <input class="form-check-input"
-                                                                                                                                                                  type="checkbox"
-                                                                                                                                                                  value=""
-                                                                                                                                                                  id="flexCheckChecked"
-                                                                                                                                                                  checked>
-                                                                                                                                                <label class="form-check-label"
-                                                                                                                                                                  for="flexCheckChecked">
-                                                                                                                                                                  Save
-                                                                                                                                                                  credentials
-                                                                                                                                                </label>
-                                                                                                                              </div>
-                                                                                                            </div>
-                                                                                                            <button class="btn btn-block btn-primary mb-4">Signin</button>
-                                                                                                            <p
-                                                                                                                              class="mb-0 text-muted">
-                                                                                                                              Don’t
-                                                                                                                              have
-                                                                                                                              an
-                                                                                                                              account?
-                                                                                                                              <a href="auth-signup.html"
-                                                                                                                                                class="f-w-400">Signup</a>
-                                                                                                            </p>
-                                                                                          </div>
-                                                                        </div>
-                                                      </div>
+<body class="bg-login">
+    <!--wrapper-->
+    <div class="wrapper">
+        <div class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0">
+            <div class="container-fluid">
+                <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
+                    <div class="col mx-auto">
+                        <div class="mb-4 text-center">
+                            <img src="assets/images/logo-img.png" width="180" alt="" />
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="border p-4 rounded">
+                                    <div class="text-center">
+                                        <h3 class="">Sign in</h3>
                                     </div>
-                  </div>
-</div>
-<!-- [ auth-signin ] end -->
+                                    <div class="login-separater text-center mb-4"> <span>OR SIGN IN WITH EMAIL</span>
+                                        <hr />
+                                    </div>
+                                    <div class="form-body">
+                                        <form class="row g-3">
+                                            <div class="col-12">
+                                                <label for="inputEmailAddress" class="form-label">Email Address</label>
+                                                <input type="email" class="form-control" id="inputEmailAddress" placeholder="Email Address">
+                                            </div>
+                                            <div class="col-12">
+                                                <label for="inputChoosePassword" class="form-label">Enter
+                                                    Password</label>
+                                                <div class="input-group" id="show_hide_password">
+                                                    <input type="password" class="form-control border-end-0" id="inputChoosePassword" value="12345678" placeholder="Enter Password"> <a href="javascript:;" class="input-group-text bg-transparent"><i class='bx bx-hide'></i></a>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-check form-switch">
+                                                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+                                                    <label class="form-check-label" for="flexSwitchCheckChecked">Remember Me</label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 text-end"> <a href="authentication-forgot-password.html">Forgot Password ?</a>
+                                            </div>
+                                            <div class="col-12">
+                                                <div class="d-grid">
+                                                    <button type="submit" class="btn btn-primary"><i class="bx bxs-lock-open"></i>Sign in</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--end row-->
+            </div>
+        </div>
+    </div>
+    <!--end wrapper-->
+    <!-- Bootstrap JS -->
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <!--plugins-->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/plugins/simplebar/js/simplebar.min.js"></script>
+    <script src="assets/plugins/metismenu/js/metisMenu.min.js"></script>
+    <script src="assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+    <!--Password show & hide js -->
+    <script>
+        $(document).ready(function() {
+            $("#show_hide_password a").on('click', function(event) {
+                event.preventDefault();
+                if ($('#show_hide_password input').attr("type") == "text") {
+                    $('#show_hide_password input').attr('type', 'password');
+                    $('#show_hide_password i').addClass("bx-hide");
+                    $('#show_hide_password i').removeClass("bx-show");
+                } else if ($('#show_hide_password input').attr("type") == "password") {
+                    $('#show_hide_password input').attr('type', 'text');
+                    $('#show_hide_password i').removeClass("bx-hide");
+                    $('#show_hide_password i').addClass("bx-show");
+                }
+            });
+        });
 
-<!-- Required Js -->
-<script src="/assets/js/vendor-all.min.js"></script>
-<script src="/assets/js/plugins/bootstrap.min.js"></script>
-<script src="/assets/js/plugins/feather.min.js"></script>
-<script src="/assets/js/pcoded.min.js"></script>
-<script>
-$("body").append(
-                  '<div class="fixed-button active"><a href="https://gumroad.com/dashboardkit" target="_blank" class="btn btn-md btn-success"><i class="material-icons-two-tone text-white">shopping_cart</i> Upgrade To Pro</a> </div>'
-                  );
-</script>
-
-
+        function login(){
+            fetch('logar/dsfkjhkdsfd', )
+        }
+    </script>
+    <!--app JS-->
+    <script src="assets/js/app.js"></script>
 </body>
 
 </html>
--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -25,7 +25,7 @@
 	<link rel="stylesheet" href="/assets/css/dark-theme.css" />
 	<link rel="stylesheet" href="/assets/css/semi-dark.css" />
 	<link rel="stylesheet" href="/assets/css/header-colors.css" />
-	<title>Kilamba</title>
+	<title>Taxi Dashboard</title>
 </head>
 
 <body>
